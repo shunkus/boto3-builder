ffdefcc44e3a1e7532cd51c8e769ea299c1135c1
shunku <s061064@gmail.com>
2025-06-09 05:21:15 +0900

    Add Emotion dependencies and fix icon imports

--- components/available-commands.component.js
+++ components/available-commands.component.js
@@ -11,7 +11,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import SearchFieldComponent from "./search-field.component";
 import CommandGuideComponent from "./command-guide.component";
-import { Info, InfoOutlined, InfoRounded } from "@mui/icons-material";
+import Info from "@mui/icons-material/Info";
+import InfoOutlined from "@mui/icons-material/InfoOutlined";
+import InfoRounded from "@mui/icons-material/InfoRounded";
 
 async function getCommands(service) {
   const response = await axios.get(`/api/${service.name}/available-commands`, {
